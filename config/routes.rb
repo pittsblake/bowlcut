e7c15a4e5911375eca2f5d4fbf76e8af23cd5210
pittsblake <pittsblake1@gmail.com>
2017-11-10 14:22:44 -0500

    created react app and heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    get "/users", to: "users#index", as: "users"
+  end
 end
