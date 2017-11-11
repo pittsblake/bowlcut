b6d02d1433fd735178f8c6c3405dce7f1fdd252d
pittsblake <pittsblake1@gmail.com>
2017-11-11 15:31:47 -0500

    Description on Stylist show page can now be edited and saved

--- client/src/components/StylistProfilePage.jsx
+++ client/src/components/StylistProfilePage.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import axios from 'axios'
+import styled from 'styled-components'
 
 class StylistProfilePage extends Component {
-    state={
+    state = {
         stylist: {}
     }
 
@@ -16,12 +17,46 @@ class StylistProfilePage extends Component {
         this.setState({ stylist: res.data })
     }
 
+    handleChange = (event) => {
+        const attribute = event.target.name
+        const updateStylist = { ...this.state.stylist }
+        updateStylist[attribute] = event.target.value
+        this.setState({ stylist: updateStylist })
+    }
+
+    handleSubmit = async (event) => {
+        event.preventDefault()
+
+        const payload = {
+            description: this.state.stylist.description
+        }
+
+        const res = await axios.patch(`/api/stylists/13`, payload)
+        console.log(res.data)
+
+        await this.setState({ stylist: res.data })
+    }
+
+
     render() {
         return (
             <div>
-                <img src={this.state.stylist.image} alt="Profile picture"/>
+                <img src={this.state.stylist.image} alt="Profile picture" />
                 <h2>{this.state.stylist.name}</h2>
-                <h3>{this.state.stylist.description}</h3>
+                <button>Ready to Cut</button>
+                <div>
+                    <textarea
+                        onSubmit={this.handleSubmit}
+                        onChange={this.handleChange}
+                        name="description"
+                        cols="100"
+                        rows="10"
+                        value={this.state.stylist.description}>
+                    </textarea>
+                </div>
+                <button>Edit</button>
+                <button>Submit</button>
+
             </div>
         );
     }
