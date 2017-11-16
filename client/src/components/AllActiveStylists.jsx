1adf4858d241e119b827b124fa15db222470fe27
pittsblake <pittsblake1@gmail.com>
2017-11-15 19:42:20 -0500

    Styled All Active stylist page

--- client/src/components/AllActiveStylists.jsx
+++ client/src/components/AllActiveStylists.jsx
@@ -4,11 +4,59 @@ import { Link, Redirect } from 'react-router-dom'
 import styled from 'styled-components'
 import StylistShowPage from './StylistShowPage'
 
+
+const NavBar = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    padding: 10px;
+    margin-right: 10px
+`
+
+const BackgroundImage = styled.div`
+    background-image: url("https://i.imgur.com/TiEuMyG.jpg");
+    background-repeat:no-repeat;
+    background-size:cover;
+    background-position:center;
+    height: 100vh;
+    width: 100%;
+    opacity: 0.7;
+`
+
 const AllStylist = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: space-around;
+    align-items: center;
+    background-color: white;
+    margin: 150px auto;
+    height: 450px;
+    z-index: auto
 `
+const Button = styled.button`
+    text-decoration: none;
+    border: none
+`
+const CenterStylistName = styled.div`
+    display: flex;
+    justify-content: center;
+    font-family: 'Josefin Sans', sans-serif
+`
+const StylistContainer = styled.div`
+    
+`
+const Image = styled.img`
+    height: 40vh;
+    max-width: 35vw;
+    &:hover {
+        height: 45vh;
+        box-shadow: 2px 4px 5px grey; 
+        cursor: grab;
+    }
+    &:active {
+        cursor: grabbing;
+    }
+`
+
 
 class AllActiveStylists extends Component {
 
@@ -71,25 +119,30 @@ class AllActiveStylists extends Component {
     }
 
     render() {
-        if (this.state.redirectToAppointmentPage){
-           return <Redirect to={`/appointment/${this.state.appointment.id}`} />
+        if (this.state.redirectToAppointmentPage) {
+            return <Redirect to={`/appointment/${this.state.appointment.id}`} />
         }
         return (
-            <div>
-                <Link to="/user/9"> Profile </Link>
-
+            <BackgroundImage>
+                <NavBar>
+                    <Link to="/user/9"> Profile </Link>
+                </NavBar>
                 <AllStylist>
                     {this.state.activeStylists.map((stylist) => {
                         return (
                             <div>
-                                <button key={stylist.id} onClick={() => this.getStylist(stylist.id)}>
-                                    {stylist.name}
-                                </button>
+                                <Button key={stylist.id} onClick={() => this.getStylist(stylist.id)}>
+                                    <Image src={stylist.image} alt="Stylist Pic" />
+                                </Button>
+                                <CenterStylistName>
+                                    <h1>{stylist.name}</h1>
+                                </CenterStylistName>
 
                             </div>
                         )
                     })}
                 </AllStylist>
+
                 {
                     this.state.showStylistShowPage ? <StylistShowPage
                         stylist={this.state.stylist.stylist}
@@ -98,7 +151,7 @@ class AllActiveStylists extends Component {
                         setAppointmentState={this.setAppointmentState}
                     /> : null
                 }
-            </div>
+            </BackgroundImage>
         );
     }
 }
