ffc619dd700981fe66a9f153e0c54736a740c689
pittsblake <pittsblake1@gmail.com>
2017-11-16 10:08:25 -0500

    Added a card to Stylist show page

--- client/src/App.js
+++ client/src/App.js
@@ -6,42 +6,45 @@ import StylistShowPage from './components/StylistShowPage'
 import StylistProfilePage from './components/StylistProfilePage'
 import UserProfilePage from './components/UserProfilePage'
 import AppointmentPage from './components/AppointmentPage'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div>
-          <Switch>
-            <Route
-              exact path="/"
-              component={HomePage}
-            />
-            <Route
-              exact path="/stylists"
-              component={AllActiveStylists}
-            />
-            <Route
-              exact path="/stylists/:id"
-              component={StylistShowPage}
-            />
-            <Route 
-              exact path="/stylist/12"
-              component={StylistProfilePage}
-            />
-            <Route
-              exact path="/user/9"
-              component={UserProfilePage}
-            />
+      <MuiThemeProvider>
+        <Router>
+          <div>
+            <Switch>
+              <Route
+                exact path="/"
+                component={HomePage}
+              />
+              <Route
+                exact path="/stylists"
+                component={AllActiveStylists}
+              />
+              <Route
+                exact path="/stylists/:id"
+                component={StylistShowPage}
+              />
+              <Route
+                exact path="/stylist/12"
+                component={StylistProfilePage}
+              />
+              <Route
+                exact path="/user/9"
+                component={UserProfilePage}
+              />
 
-            <Route
-              exact path="/appointment/:id"
-              component={AppointmentPage}
-            />
+              <Route
+                exact path="/appointment/:id"
+                component={AppointmentPage}
+              />
 
-          </Switch>
-        </div>
-      </Router>
+            </Switch>
+          </div>
+        </Router>
+      </MuiThemeProvider>
     );
   }
 }
--- client/src/components/StylistShowPage.jsx
+++ client/src/components/StylistShowPage.jsx
@@ -2,26 +2,27 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 import styled from 'styled-components'
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 
-const CardContainer = styled.div`
-  position: relative;
-  z-index: 1;
-  margin: 32px auto;
-  max-width: 720px;
-  height: 420px;
-  perspective: 1000px;
-`
-const Card = styled.div`
-  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-  transition: 0.3s;
-  border-radius: 5px;
+// const CardContainer = styled.div`
+//   position: relative;
+//   z-index: 1;
+//   margin: 32px auto;
+//   max-width: 720px;
+//   height: 420px;
+//   perspective: 1000px;
+// `
+// const Card = styled.div`
+//   box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+//   transition: 0.3s;
+//   border-radius: 5px;
 
-`
+// `
 
-const Image = styled.img`
-  max-width: 100px;
-  max-height: 100px;
-`
+// const Image = styled.img`
+//   max-width: 100px;
+//   max-height: 100px;
+// `
 
 
 const StylistShowPage = (props) => {
@@ -47,20 +48,20 @@ const StylistShowPage = (props) => {
 
     return (
         <Card>
-            <CardContainer>
+            <div>
                 <div>
 
                     <button onClick={() => createAppointment()}>
                         Book it
                     </button>
                     
-                    <Image src={props.stylist.image} alt="Profile Picture" />
+                    <img src={props.stylist.image} alt="Profile Picture" />
                     
                     <h1>{props.stylist.name}</h1>
                     <h3>{props.stylist.description}</h3>
 
                 </div>
-            </CardContainer>
+            </div>
         </Card>
     );
 }
