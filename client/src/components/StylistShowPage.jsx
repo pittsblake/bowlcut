94c88b90dc3213c29c4504d1f701cd4d3e529d02
pittsblake <pittsblake1@gmail.com>
2017-11-16 09:52:42 -0500

    installed material-ui

--- client/src/components/StylistShowPage.jsx
+++ client/src/components/StylistShowPage.jsx
@@ -1,6 +1,27 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import axios from 'axios'
+import styled from 'styled-components'
+
+const CardContainer = styled.div`
+  position: relative;
+  z-index: 1;
+  margin: 32px auto;
+  max-width: 720px;
+  height: 420px;
+  perspective: 1000px;
+`
+const Card = styled.div`
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  border-radius: 5px;
+
+`
+
+const Image = styled.img`
+  max-width: 100px;
+  max-height: 100px;
+`
 
 
 const StylistShowPage = (props) => {
@@ -23,15 +44,24 @@ const StylistShowPage = (props) => {
         }
     }
 
-    
+
     return (
-        <div>
-            <button onClick={() => createAppointment()}>
-                Book it
-            </button>
-            <h1>{props.stylist.name}</h1>
-            <h3>{props.stylist.description}</h3>
-        </div>
+        <Card>
+            <CardContainer>
+                <div>
+
+                    <button onClick={() => createAppointment()}>
+                        Book it
+                    </button>
+                    
+                    <Image src={props.stylist.image} alt="Profile Picture" />
+                    
+                    <h1>{props.stylist.name}</h1>
+                    <h3>{props.stylist.description}</h3>
+
+                </div>
+            </CardContainer>
+        </Card>
     );
 }
 
